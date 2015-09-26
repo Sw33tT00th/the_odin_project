637e0287365e1e67f7c02a2ac975cf01c0985938
John Loudon <jsl245@nau.edu>
2015-09-26 08:10:02 -0700

    optimized prime problem further

--- Javascript_Problems/problem3.js
+++ Javascript_Problems/problem3.js
@@ -8,6 +8,7 @@
 	of optimization. It still largely uses the brute force method,
 	but instead of checking from the number down it starts at half
 	of the number.
+	
 	We can do this because if there is at least 1 prime factor that
 	isn't the number itself, then there must be at least 2 prime
 	factors. Since the lowest prime value possible is 2, that means
@@ -15,16 +16,46 @@
 	What this means is that it cuts runtime in half, which still isn't
 	great on the 6 year old laptop I wrote this on.
 
+	I optimized a bit more based on this idea. If the number itself is
+	not prime, then there must be at least 2 prime factors. Since that
+	is the case, we first find the smallest prime factor (which is 
+	generally much faster) and reduce the initial number by that factor.
+	The result of that calculation gives us the highest possible value
+	for the largest prime factor. From there we brute force it.
+
+	This method dramatically improves runtime, but the improvement
+	varies wildly. A test on the number 600851475 on this computer took
+	6.55 seconds, while a test on the larger number 6008514751 took 20ms.
+	The significant contributing factor being how large the smallest prime
+	factor is. The larger the smallest prime is, the fewer possibilities
+	the brute force portion needs to check. Worst case runtime being n/2
+	which it will ALWAYS get if n is even.
+
 	IMPORTANT NOTE: I tested this through a unix terminal. If you want
 	to test this in a browser you will need to modify output.
 */
+
+var smallestPrime = function (number) {
+	if (isPrime(number)) {
+		return number;
+	}
+	var maxVal = Math.ceil(Math.sqrt(number));
+	// check if any number less than the square root divides evenly
+	// into the number in question.
+	for (var i = 2; i < maxVal; i++) {
+		if (number % i === 0) {
+			// if an even division is found the number is not prime.
+			return i;
+		}
+	}
+}
 
 // check to see if a number is prime or not
 var isPrime = function (number) {
 	var maxVal = Math.ceil(Math.sqrt(number));
 	// check if any number less than the square root divides evenly
 	// into the number in question.
-	for (var i = 2; i < maxVal; i++){
+	for (var i = 2; i < maxVal; i++) {
 		if (number % i === 0) {
 			// if an even division is found the number is not prime.
 			return false;
@@ -38,7 +69,7 @@ var findMaxPrime = function (number) {
 	if (isPrime(number)) {
 		return number;
 	}
-	var factor = Math.ceil(number / 2);
+	var factor = number / smallestPrime(number);
 	while (factor > 1) {
 		if (number % factor === 0 && isPrime(factor)) {
 			return factor;
@@ -48,7 +79,7 @@ var findMaxPrime = function (number) {
 	return number;
 }
 
-var num = 600851475;
+var num = 6008514751;
 
 var start = new Date().getTime();
 print('Answer: ' + findMaxPrime(num));
